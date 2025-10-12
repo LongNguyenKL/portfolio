50f63b326d0084f267527b104f08c831a92f47e7
Long Nguyen <longng2809@gmail.com>
2025-10-11 23:49:26 -0400

    Edit contact form

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -57,10 +57,10 @@ function Contact() {
 
   return (
     <div id="contact">
-      <div className="items-container">
+      <div className="form-items-container">
         <div className="contact_wrapper">
           <h1>Contact Me</h1>
-          <p>Got a project waiting to be realized? Let's collaborate and make it happen!</p>
+          <p>Interested in partnering with me for your data analytics opportunity? Let's discuss!</p>
           <Box
             ref={form}
             component="form"
